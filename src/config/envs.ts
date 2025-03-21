454d595266d90e8db68d17b045771525b3cac58f
Carlos Motino <carlos.motino911@gmail.com>
2025-03-21 16:41:52 -0600

    Initial commit

---
+++ src/config/envs.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  STRIPE_SECRET: string;
+  STRIPE_SUCCESS_URL: string;
+  STRIPE_CANCEL_URL: string;
+  STRIPE_ENDPOINT_SECRET: string;
+}
+
+export const envsSchema = joi
+  .object<EnvVars>({
+    PORT: joi.number().required(),
+    STRIPE_SECRET: joi.string().required(),
+    STRIPE_SUCCESS_URL: joi.string().required(),
+    STRIPE_CANCEL_URL: joi.string().required(),
+    STRIPE_ENDPOINT_SECRET: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envsVars: EnvVars = value;
+
+export const envs = {
+  port: envsVars.PORT,
+  stripeSecret: envsVars.STRIPE_SECRET,
+  stripeSuccessUrl: envsVars.STRIPE_SUCCESS_URL,
+  stripeCancelUrl: envsVars.STRIPE_CANCEL_URL,
+  stripeEndpointSecret: envsVars.STRIPE_ENDPOINT_SECRET,
+};
